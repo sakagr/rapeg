7f120eefb34b6f0b812f698d7c7913285d89a2fe
sakagr <ska70836@gmail.com>
2018-11-13 03:24:42 +0530

    Add files via upload

--- assets/php/mailer.php
+++ assets/php/mailer.php
@@ -1,3 +1,17 @@
 <?php
-echo "Coming Soon! -- This Script wil help in mailing...";
+//echo "Coming Soon! -- This Script wil help in mailing...";
+if(isset($_POST['submit']))
+{
+	$uname = $_POST['username'];
+	$email = $_POST['email_address'];
+	$sub = $_POST['subject'];
+	$msg = $_POST['messages'];
+	$headers = "From: $email";
+	$msg = "From: ".$uname."<br><br>".$msg;
+	
+	mail("ska70836@gmail.com",$sub,$msg,$headers);
+
+}
+
+
 ?>

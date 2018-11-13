f1a3c1e6dc9d57de43984e7abd791858444762f8
sakagr <ska70836@gmail.com>
2018-11-14 00:04:50 +0530

    Add files via upload

--- assets/php/mailer.php
+++ assets/php/mailer.php
@@ -1,3 +1,5 @@
+<html>
+	<body>
 <?php
 //echo "Coming Soon! -- This Script wil help in mailing...";
 if(isset($_POST['submit']))
@@ -7,7 +9,7 @@ if(isset($_POST['submit']))
 	$sub = $_POST['subject'];
 	$msg = $_POST['messages'];
 	$headers = "From: $email";
-	$msg = "From: ".$uname."<br><br>".$msg;
+	$msg = "From: "$uname."<br><br>".$msg;
 	
 	mail("ska70836@gmail.com",$sub,$msg,$headers);
 
@@ -15,3 +17,5 @@ if(isset($_POST['submit']))
 
 
 ?>
+	</body>
+	</html>
---
+++ php/tracker.php
@@ -0,0 +1,3 @@
+<?php
+echo "You will see us shortly! -- This Script will help in tracking items...";
+?>
